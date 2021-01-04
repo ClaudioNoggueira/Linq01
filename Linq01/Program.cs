0f2de5ef0db98d37eb04e4d61833a03e5c35732d
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-04 14:12:15 -0300

    First commit

---
+++ Linq01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq01 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Specify the data source
+            int[] numbers = new int[] { 3, 4, 5, 6 };
+
+            //Define the query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            //Execute the query
+            foreach (int item in result) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
